3133421d3bee757403361ecfbbb017d1071d752d
Noah Calvo <web@sweetleavesnorthloop.com>
2026-05-04 16:42:20 -0500

    Revert "Update middleware.ts"
    This reverts commit 42472cbcb3a23ecf51687444e2d5579064aafd69.

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const canonicalUrl = process.env.NEXT_PUBLIC_SITE_URL;
+
+export function middleware(request: NextRequest) {
+  if (!canonicalUrl) return;
+
+  const host = request.headers.get("host") || "";
+  if (host.endsWith(".vercel.app") || host.startsWith("localhost")) return;
+
+  const canonical = new URL(canonicalUrl);
+  if (host === canonical.host) return;
+
+  const url = request.nextUrl.clone();
+  url.host = canonical.host;
+  url.protocol = canonical.protocol;
+  url.port = canonical.port;
+  return NextResponse.redirect(url, 301);
+}
+
 export const config = {
-  matcher: [],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
