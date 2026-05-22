ac6385e6036435a6af0930b86876ab698a96da79
Noah Calvo <noah.calvo@gmail.com>
2026-05-22 15:26:56 -0500

    avoid inner scroll

--- app/loyalty/components/SignUpSection.tsx
+++ app/loyalty/components/SignUpSection.tsx
@@ -2,7 +2,7 @@ export default function SignUpSection() {
   return (
     <div
       id="signup"
-      className="bg-dark-green rounded-[50px] flex flex-col items-center w-full h-full min-h-[1600px] sm:min-h-[1500px] md:min-h-[1450px] lg:min-h-0 overflow-hidden"
+      className="bg-dark-green rounded-[50px] flex flex-col items-center w-full h-full min-h-[1700px] sm:min-h-[1500px] md:min-h-[1450px] lg:min-h-0 overflow-hidden"
     >
       <h2 className="font-poppins-bold text-3xl md:text-display text-white text-center px-6 md:px-8 pt-10 md:pt-12 leading-tight">
         Sign Up
